d031896b722e675eb3c0b2cfb4d4f91c0905281a
Chuk <chukmunnlee@gmail.com>
2017-10-24 13:28:09 +0800

    changed cenv.bat

--- cenv.bat
+++ cenv.bat
@@ -1,19 +1,22 @@
 REM Set Android Home
-set ANDROID_HOME=C:\Program Files (x86)\Android\android-sdk
-set ANDROID_PATH=%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
+set ANDROID_HOME=%USERPROFILE%\AppData\Local\Android\sdk
+set ANDROID_PATH=%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%ANDROID_HOME%\build-tools\26.0.2
 
 REM Set NodeJS
-set NODEJS=C:\Program Files\nodejs
+REM set NODEJS=C:\Program Files\nodejs
+
+REM Set Gradle
+set GRADLE_PATH="C:\Program Files\Android\Android Studio\gradle\gradle-3.2\bin"
 
 REM Set NodeJS Modules
-set NODEJS_MODULES=%USERPROFILE%\AppData\Roaming\npm
+REM set NODEJS_MODULES=%USERPROFILE%\AppData\Roaming\npm
 
 REM Set Git
-set GIT_HOME=C:\Program Files (x86)\Git
-set GIT_PATH=%GIT_HOME%\bin
+REM set GIT_HOME=C:\Program Files (x86)\Git
+REM set GIT_PATH=%GIT_HOME%\bin
 
 REM Set JDK
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_112
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
 set JAVA_PATH=%JAVA_HOME%\bin
 
-set PATH=%NODEJS_MODULES%;%ANDROID_PATH%;%NODEJS%;%JAVA_PATH%;%GIT_PATH%;%PATH%
+set PATH=%ANDROID_PATH%;%JAVA_PATH%;%GRADLE_PATH%;%PATH%
